d66c9843cdc33bd3e5c20955e396458cdc863087
Bruno Alexandre Soares Prestes <brunoasprestes@gmail.com>
2026-02-05 13:44:47 -0300

    correcoes typescript

--- components/BtnAction.tsx
+++ components/BtnAction.tsx
@@ -22,7 +22,7 @@ const BtnAction = ({ caption, styles, href, target, rel }: BtnProps) => {
   const motionProps = {
     whileHover: { scale: 1.03 },
     whileTap: { scale: 0.98 },
-    transition: { type: "spring", stiffness: 400, damping: 17 },
+    transition: { type: "spring" as const, stiffness: 400, damping: 17 },
   };
 
   return (
